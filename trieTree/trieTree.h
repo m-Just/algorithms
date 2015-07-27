ca5353f561fcee3dc4c414de190d0af157a2178c
m-Just <mjust.lkc@gmail.com>
2015-07-27 21:56:33 +0800

    Update trieTree.h

--- trieTree/trieTree.h
+++ trieTree/trieTree.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node {
+  int letter;        // ascii code
+  int appearance;
+  struct node *(next[30]); // pointing to the start of an array of all next letters
+  int size;
+};
+
+typedef struct node treeNode;
