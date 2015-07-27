746b5591e93e6f91fd0590ff0c3af68bfd2bf72f
m-Just <mjust.lkc@gmail.com>
2015-07-27 21:49:18 +0800

    Create testMain.c

---
+++ fastExp/testMain.c
@@ -0,0 +1,11 @@
+#include <fastExp.h>
+
+int main() {
+  long long base, ans;
+  int power;
+  
+  scanf("%lld %d", &base, &power);
+  if (fastExp(base, power, &ans)) printf("%lld\n", ans);
+  else return -1;
+  return 0;
+}
