8458957f4aeca96c0372272d9147448ea5c71d1b
m-Just <mjust.lkc@gmail.com>
2015-08-07 12:56:52 +0800

    Create gaussElimination.h

---
+++ Gauss-Jordan-Matrix-Elimination/gaussElimination.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int solve(double* a, double* y, int h, int w, double* buff);
+int getTriMatrix(double* origin, double* originY, int h, int w, double* buff, double* buffY);
+int bubbleSort(double* num, int* label, int length);
+void swap(void* a, void* b);
